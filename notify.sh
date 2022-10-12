f05b5a4a901e9d0c80c649f62453e7e550cc9896
WWWWM03 <74545085+WWWWM03@users.noreply.github.com>
2022-10-12 23:04:14 +0800

    更新镜像地址dockerhub跳转tag页面。
    由 @Alano-i 提供

--- notify.sh
+++ notify.sh
@@ -11,6 +11,10 @@ export DIUN_ENTRY_DIGEST111=$DIUN_ENTRY_DIGEST
 export DIUN_ENTRY_CREATED111=$DIUN_ENTRY_CREATED
 export DIUN_ENTRY_PLATFORM111=$DIUN_ENTRY_PLATFORM
 
+if [ ["${DIUN_ENTRY_HUBLINK111}"= "https://registry.hub.docker"*] ]; then
+    DIUN_ENTRY_HUBLINK111="${DIUN_ENTRY_HUBLINK111}/tags"
+fi
+
 imagename1="$(echo "$DIUN_ENTRY_IMAGE111" | cut -d / -f3)"
 imagename2="$(echo "$DIUN_ENTRY_IMAGE111" | cut -d / -f2-3)"
 imagename3="$(echo "$imagename1" | cut -d : -f1)"
